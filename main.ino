60b2448c1ca37d6806e3dccc85b6c79a8dbcfed0
valmyr <65004196+valmyr@users.noreply.github.com>
2021-03-02 21:16:09 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -8,7 +8,7 @@
 #define SensorCondutividadeEletrica A1
 #define SensorDeTemperaturaUmidade A2
 #define SensorUmidadeDoSoloContinua A3
-#define SensorUmidadeDoSoloGotijamento A4
+#define SensorUmidadeDoSoloGotejamento A4
 #define relogio1 2
 #define relogio2 3
 #define AcionamentoBombaContinua 4
@@ -20,18 +20,18 @@
 //vazão =  dv / dt
 float tempoQueAbombaFicaraLigada = float(volumeDeAguaDaIrrigacao)/float((vazaoDaBomba/(60*60)));//Em segundos
 long int tempoInicialBombaContinua;bool capituraContinuaTempo = true;
-long int tempoInicialBombaGotijamentoEmMinutos,tempoInicialBombaGotijamento;bool capituraGotijamentoTempo = true;
-int numeroDeLigacoesBombaGotijamento = 0;
+long int tempoInicialBombaGotejamentoEmMinutos,tempoInicialBombaGotejamento;bool capituraGotejamentoTempo = true;
+int numeroDeLigacoesBombaGotejamento = 0;
 SensorPH sensor(SensorPHPorte);
 SensorCondutividade condutividadeEletrica(SensorCondutividadeEletrica);
 SensorDeUmidadeSolo UmidadeSoloContinua(SensorUmidadeDoSoloContinua);
-SensorDeUmidadeSolo UmidadeSoloGotijamento(SensorUmidadeDoSoloGotijamento);
+SensorDeUmidadeSolo UmidadeSoloGotejamento(SensorUmidadeDoSoloGotejamento);
 
 dht DHT;
 DS1307 relogio(relogio1, relogio2);
 
 bool acionamentoDaBombaContinua(long int);
-bool acionamentoDaBombaGotijamento(long int);
+bool acionamentoDaBombaGotejamento(long int);
 
 double temperaturaAmbiente(){
     delay(2000);
@@ -68,26 +68,26 @@ void loop() {
         capituraContinuaTempo = true;
     }
 
-    if(float(umidadeDeCampo - umidadeDeCampo*.075) <  UmidadeSoloGotijamento.getUmidade() < float(umidadeDeCampo + umidadeDeCampo*.075) and numeroDeLigacoesBombaGotijamento <= 6){
-        //Bloco Bomba Gotijamento
-        if(capituraGotijamentoTempo) {
-            tempoInicialBombaGotijamentoEmMinutos = relogio.getTime().min;
-            tempoInicialBombaGotijamento = millis();
-            capituraGotijamentoTempo = false;
+    if(float(umidadeDeCampo - umidadeDeCampo*.075) <  UmidadeSoloGotejamento.getUmidade() < float(umidadeDeCampo + umidadeDeCampo*.075) and numeroDeLigacoesBombaGotejamento <= 6){
+        //Bloco Bomba Gotejamento
+        if(capituraGotejamentoTempo) {
+            tempoInicialBombaGotejamentoEmMinutos = relogio.getTime().min;
+            tempoInicialBombaGotejamento = millis();
+            capituraGotejamentoTempo = false;
         }else{
-            if(relogio.getTime().min - tempoInicialBombaGotijamentoEmMinutos >= 30) {
-                tempoInicialBombaGotijamento = millis();
-                numeroDeLigacoesBombaGotijamento+=1;
+            if(relogio.getTime().min - tempoInicialBombaGotejamentoEmMinutos >= 30) {
+                tempoInicialBombaGotejamento = millis();
+                numeroDeLigacoesBombaGotejamento+=1;
             }else{
-                if(relogio.getTime().min - tempoInicialBombaGotijamentoEmMinutos < 0 ){
-                    tempoInicialBombaGotijamentoEmMinutos = 30 - tempoInicialBombaGotijamentoEmMinutos + (relogio.getTime().min-1);
+                if(relogio.getTime().min - tempoInicialBombaGotejamentoEmMinutos < 0 ){
+                    tempoInicialBombaGotejamentoEmMinutos = 30 - tempoInicialBombaGotejamentoEmMinutos + (relogio.getTime().min-1);
                 }
             }
-            acionamentoDaBombaGotijamento(tempoInicialBombaGotijamento);
+            acionamentoDaBombaGotejamento(tempoInicialBombaGotejamento);
         }
     }else{
-        capituraGotijamentoTempo = true;
-        numeroDeLigacoesBombaGotijamento = 0;
+        capituraGotejamentoTempo = true;
+        numeroDeLigacoesBombaGotejamento = 0;
     }
 
 }
@@ -99,7 +99,7 @@ bool acionamentoDaBombaContinua(long int tempoInicial){
     digitalWrite(AcionamentoBombaContinua,LOW);
     return false;
 }
-bool acionamentoDaBombaGotijamento(long int tempoInicial){
+bool acionamentoDaBombaGotejamento(long int tempoInicial){
     if(micros()*pow(10,3) - tempoInicial > tempoQueAbombaFicaraLigada/6){
         digitalWrite(AcionamentoBombaGotejamento,HIGH);
         return true;
